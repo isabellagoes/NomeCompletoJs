ad2f4622af4a8c65689394da2d95df1bc9bed34b
isabellagoes <isabellagoes.igs@gmail.com>
2024-04-18 22:22:31 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let nome = prompt("Digite seu nome");
 let sobrenome = prompt("Digite seu sobrenome");
 
-alert ("Seu nome completo é " + nome + sobrenome);
+let nomeCompleto = nome + " " + sobrenome;
+let nomeCatalogo = (sobrenome.toUpperCase() + "," + nome);
+
+alert ("Seu nome completo é ");
 alert (sobrenome.toUpperCase() + "," + nome);
