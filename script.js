7306752e5221aa5b8b4aee64a185dc6a731596ed
isabellagoes <isabellagoes.igs@gmail.com>
2024-04-21 12:03:30 -0300

    me baseei no exercício de csharp e misturei as linguagens :(

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+// Console.WriteLine("Insira a seguir:");
+// Console.WriteLine();
+
+// Console.Write("Seu nome: ");
+// string nome;
+// nome = Console.ReadLine()!;
+
+// Console.Write("Seu sobrenome: ");
+// string sobrenome;
+// sobrenome = Console.ReadLine()!;
+
+// Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+// Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+
 let nome = prompt("Digite seu nome");
 let sobrenome = prompt("Digite seu sobrenome");
 
-let nomeCompleto = nome + " " + sobrenome;
-let nomeCatalogo = (sobrenome.toUpperCase() + "," + nome);
+// let nomeCompleto = nome + " " + sobrenome;
+// let nomeCatalogo = sobrenome.toUpperCase() + "," + nome;
 
-alert ("Seu nome completo é ");
-alert (sobrenome.toUpperCase() + "," + nome);
+alert('Seu nome completo é: ');
+alert(nome + " " + sobrenome);
+
+alert('Seu nome de catálogo é: ');
+alert(sobrenome.toUpperCase() + ", " + nome);
