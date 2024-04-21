60adfe6f8109bc89cb159085c2c6ef3cefa8f71c
isabellagoes <isabellagoes.igs@gmail.com>
2024-04-21 12:20:24 -0300

    remoção de quebra de resultado no alert

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-// Console.WriteLine("Insira a seguir:");
-// Console.WriteLine();
-
-// Console.Write("Seu nome: ");
-// string nome;
-// nome = Console.ReadLine()!;
-
-// Console.Write("Seu sobrenome: ");
-// string sobrenome;
-// sobrenome = Console.ReadLine()!;
-
-// Console.WriteLine($"Nome completo: {nome} {sobrenome}");
-// Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
-
 let nome = prompt("Digite seu nome");
 let sobrenome = prompt("Digite seu sobrenome");
 
-// let nomeCompleto = nome + " " + sobrenome;
-// let nomeCatalogo = sobrenome.toUpperCase() + "," + nome;
+alert('Seu nome completo é: ' + nome + " " + sobrenome);
 
-alert('Seu nome completo é: ');
-alert(nome + " " + sobrenome);
-
-alert('Seu nome de catálogo é: ');
-alert(sobrenome.toUpperCase() + ", " + nome);
+alert('Seu nome de catálogo é: ' + sobrenome.toUpperCase() + ", " + nome);
